7bb49c9c5edaa64427add0dd9cf438546d230991
Justin Pfeifler <justin.pfeifler@revunit.com>
2019-08-13 16:15:07 -0500

    Add React

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+	return (
+		<h1> Hi there!</h1>
+	)
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
