039e5206256fd7277af9498575e2145b4ccd24f3
Justin Pfeifler <justin.pfeifler@revunit.com>
2019-08-14 13:10:01 -0500

    Add index logic and styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import GetLocations from './GetLocations/GetLocations';
+import LocationGrid from './LocationGrid/LocationGrid';
+import scss from './index.scss';
+import SearchBar from './SearchBar/SearchBar';
 
 const App = () => {
 	return (
-		<h1> Hi there!</h1>
+        <div>
+            <div class="location-select-header">Choose Location</div>
+            <SearchBar />
+            <div class="location-grid">
+                <GetLocations />
+            </div>
+        </div>
 	)
 }
 
