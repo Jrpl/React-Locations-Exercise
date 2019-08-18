3e219dbdff8f3b55445fbd3042f8780708c1cc7e
Justin Pfeifler <justin.pfeifler@revunit.com>
2019-08-18 16:27:22 -0500

    Changed class component to functional

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './SearchBar.scss';
+
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const onInputChange = event => {
+        setSearchTerm(event.target.value);
+    }
+
+    const inputStyle = {
+        width: '256px'
+    }
+
+    return (
+        <div className="gray-bar">
+            <input 
+                type="text"
+                className="input"
+                style={inputStyle}
+                placeholder="Search by city or airport code" 
+                value={searchTerm}
+                onChange={(event) => onInputChange(event)}
+            />
+        </div>
+    );
+}
+
+export default SearchBar;
