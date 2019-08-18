eca20b81b51f5065cafa9fd6dc1d6dc284b899ec
Justin Pfeifler <justin.pfeifler@revunit.com>
2019-08-18 16:41:15 -0500

    Changed class component to functional

---
+++ src/Components/Locations/Locations.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import LocationCard from '../LocationCard/LocationCard';
+import axios from 'axios';
+
+const Locations = () => {
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios.get(`locations.json`);
+            setLocations(response.data.filter(filterNames));
+        })();
+    })
+
+    const filterNames = obj => (obj.name.includes("A5C") || obj.name.includes("Q5")) ? false : true;
+
+    return (
+        locations.map((location) => <LocationCard location = {location} key={location.id} />)
+    );
+}
+
+export default Locations;
